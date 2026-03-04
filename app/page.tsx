0cb6aca77724c169114c6c71a22a0ef23a82bece
jukka211 <ivan.olegovich98@gmail.com>
2026-03-04 20:37:46 +0100

    Redirect homepage to /application

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,6 @@
-import Link from "next/link";
+// /Users/jukka_w/czybik-schmid/app/page.tsx
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return (
-    <main style={{ padding: 24 }}>
-      <nav style={{ display: "flex", gap: 12, marginBottom: 24 }}>
-        <Link href="/">Home (Form for now)</Link>
-        <Link href="/application">Application</Link>
-        <Link href="/about">About</Link>
-      </nav>
-
-      <h1>Application Form</h1>
-      <p>Form comes next.</p>
-    </main>
-  );
+  redirect("/application");
 }
