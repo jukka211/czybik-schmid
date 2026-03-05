4fdedc6d6e455ffea280d5dd5df6e2858c2cee40
jukka211 <ivan.olegovich98@gmail.com>
2026-03-05 16:12:20 +0100

    Update homepage

--- app/datenschutz/page.tsx
+++ app/datenschutz/page.tsx
@@ -1,8 +1,17 @@
 export default function DatenschutzPage() {
     return (
       <main style={{ padding: 24, maxWidth: 800, margin: "0 auto" }}>
-        <h1>Datenschutzerklärung</h1>
-        <p>Coming soon.</p>
+        <h1>Datenschutzerklärung & Impressum</h1>
+        <p>
+Anbieterkennung gem. § 5 Telemediengesetz (TMG) und § 55  Rundfunkstaatsvertrag (RStV).
+Verantwortlicher im Sinne der Datenschutz-Grundverordnung (DSGVO) und der sonstigen datenschutzrechtlichen Bestimmungen ist:
+
+Czybik & Schmid Media UG (haftungsbeschränkt) i.G.
+Herr Laurin Schmid
+Kollwitzstr. 76
+10435 Berlin
++49 15255133224
+Umsatzsteuer-ID: (wird nachgereicht)</p>
       </main>
     );
   }
