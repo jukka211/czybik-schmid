fe2ef81d65d9a4474388010f2348b263508ed2a2
jukka211 <ivan.olegovich98@gmail.com>
2026-04-12 20:02:16 +0200

    “Update”

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,21 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Czybik Schmid",
-  description: "Czybik Schmid",
+  description:
+    "Seit 2018 fotografieren wir für den Bund sowie für politische, gesellschaftliche und kulturelle Formate – mit Expertise in Porträt-, Veranstaltungs-, Reportage-, PR- und Architekturfotografie.",
+  openGraph: {
+    title: "Czybik Schmid",
+    description:
+      "Seit 2018 fotografieren wir für den Bund sowie für politische, gesellschaftliche und kulturelle Formate.",
+    images: ["/opengraph-image.png"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Czybik Schmid",
+    description:
+      "Seit 2018 fotografieren wir für den Bund sowie für politische, gesellschaftliche und kulturelle Formate.",
+    images: ["/twitter-image.png"],
+  },
 };
 
 export default function RootLayout({
@@ -23,13 +37,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-<html lang="en" suppressHydrationWarning>
-<body
-  suppressHydrationWarning
-  className={`${geistSans.variable} ${geistMono.variable} antialiased`}
->
-  {children}
-</body>
+    <html lang="de" suppressHydrationWarning>
+      <body
+        suppressHydrationWarning
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
